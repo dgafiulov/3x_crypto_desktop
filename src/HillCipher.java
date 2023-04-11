b50e512e494cebdde76e888bc756e4e19163ecc1
dgafiulov <dgafiulov@gmail.com>
2023-04-11 18:33:59 +0500

    added encode functions for Hill cipher

--- src/HillCipher.java
+++ src/HillCipher.java
@@ -1,13 +1,13 @@
+import java.util.Arrays;
 import java.util.Objects;
 
 public class HillCipher {
 
 
     public byte[] encode(byte[] orig, String key) {
-
-        byte[][] keyMatrix = keyToMatrix("");
-
-        return new byte[]{};
+        byte[][] keyMatrix = keyToMatrix(key);
+        byte[] encoded = bytesEncode(orig, keyMatrix);
+        return encoded;
     }
 
     private byte[][] keyToMatrix(String key) {
@@ -23,10 +23,20 @@ public class HillCipher {
         return keyMatrix;
     }
 
-    private int[] matrixMultiply(byte[] a, byte[][] b) {
-        int[] c = new int[2];
-        c[0] = (a[0] * b[0][0] + a[1] * b[1][0]);
-        c[1] = (a[0] * b[0][1] + a[1] * b[1][1]);
+    private byte[] matrixMultiply(byte[] a, byte[][] b) {
+        byte[] c = new byte[2];
+        c[0] = (byte) ((((a[0] * b[0][0] + a[1] * b[1][0]) + 128) % 256) - 128);
+        c[1] = (byte) ((((a[0] * b[0][1] + a[1] * b[1][1]) + 128) % 256) - 128);
         return c;
+    }
+
+    private byte[] bytesEncode(byte[] bytes, byte[][] key) {
+        byte[] multiplyRes;
+        for (int i = 0; i < bytes.length; i = i + 2) {
+            multiplyRes = matrixMultiply(new byte[]{bytes[i], bytes[i + 1]}, key);
+            bytes[i] = multiplyRes[0];
+            bytes[i + 1] = multiplyRes[1];
+        }
+        return bytes;
     }
 }
